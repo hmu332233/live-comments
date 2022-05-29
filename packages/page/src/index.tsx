85a7721de4d86f63a83df1d6dfb0f3ae3cce1ba1
Minung Han <hmu332233@gmail.com>
2022-05-29 11:49:18 +0900

    nextjs에서 parcel 기반 프로젝트로 변경

---
+++ packages/page/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>test</div>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('app')
+);
