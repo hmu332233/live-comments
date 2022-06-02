4e6759a8231e309107858ccceb420f0c028a880b
Minung Han <hmu332233@gmail.com>
2022-06-02 23:59:50 +0900

    background에 firebase 로그인 추가

--- packages/extension/background.js
+++ packages/extension/background.js
@@ -1,18 +1,26 @@
+self.importScripts('firebase/firebase-app.js');
+self.importScripts('firebase/firebase-auth.js');
+
 function injectHTML() {
   document.write(`
-    <!doctype html>
-    <html>
-      <head>
-        <meta charset="utf-8"/>
-        <title>Page</title>
-        <link rel="stylesheet" href="http://localhost:1234/index.433a9dfd.css">
-        </head>
-        <body>
-        <div id="app"></div>
-      </body>
-    </html>
-  `);
+      <!doctype html>
+      <html>
+        <head>
+          <meta charset="utf-8"/>
+          <title>Page</title>
+          <link rel="stylesheet" href="http://localhost:1234/index.433a9dfd.css">
+          </head>
+          <body>
+          <div id="app"></div>
+        </body>
+      </html>
+    `);
 }
+
+const firebaseConfig = {};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const auth = firebaseApp.auth();
 
 chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
@@ -23,4 +31,32 @@ chrome.action.onClicked.addListener((tab) => {
     target: { tabId: tab.id },
     files: ['dist/index.6c92a201.js'],
   });
+  firebase
+    .auth()
+    .signInAnonymously()
+    .then((...props) => {
+      // Signed in..
+      console.log(props);
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
+
+  // chrome.identity.getAuthToken({}, (token) => {
+  //   console.log(token)
+  //   let credential = firebase.auth.GoogleAuthProvider.credential(null, token)
+  //   app
+  //     .auth()
+  //     .signInWithCredential(credential)
+  //     .then((userCredential) => {
+  //       const user = firebase.auth().currentUser
+  //       console.log(user)
+  //     })
+  //     .catch((error) => {
+  //       console.error(error)
+
+  //     })
+  // })
 });
