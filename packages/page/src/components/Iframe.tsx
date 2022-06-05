c96c53a237a505aa7d0b60547662c22987db4eee
Minung Han <hmu332233@gmail.com>
2022-06-05 17:59:32 +0900

    Iframe 컴포넌트 추가

---
+++ packages/page/src/components/Iframe.tsx
@@ -0,0 +1,23 @@
+import React, { forwardRef } from 'react';
+
+type Props = {
+  src: string;
+  onLoad: () => void;
+};
+
+function Iframe(
+  { src, onLoad }: Props,
+  ref: React.RefObject<HTMLIFrameElement>,
+) {
+  return (
+    <iframe
+      ref={ref}
+      className="w-full"
+      src={src}
+      onLoad={onLoad}
+      sandbox="allow-scripts allow-forms allow-same-origin allow-presentation allow-orientation-lock allow-modals allow-popups-to-escape-sandbox allow-pointer-lock"
+    />
+  );
+}
+
+export default forwardRef(Iframe);
