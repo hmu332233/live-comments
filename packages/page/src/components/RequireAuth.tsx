46294879d87c6f2c135a58b5785672d46c9c727a
Minung Han <hmu332233@gmail.com>
2022-06-05 17:03:23 +0900

    불필요한 useLocation 제거

--- packages/page/src/components/RequireAuth.tsx
+++ packages/page/src/components/RequireAuth.tsx
@@ -10,14 +10,11 @@ type Props = {
 function RequireAuth({ children }: Props) {
   const user = useContext(AuthStateContext);
 
-  const location = useLocation();
-
   if (!user) {
     console.log('로그인 안됨');
     // replace를 줘서 뒤로가기시 로그인 페이지로 가지 않도록 하고
     // 로그인 이후에 본래 가려고 했던 페이지로 보낼 수 있도록 pathname을 state로 넘겨준다.
-    const from = location.pathname;
-    return <Navigate to="/login" state={{ from }} replace />;
+    return <Navigate to="/login" replace />;
   }
 
   return children;
