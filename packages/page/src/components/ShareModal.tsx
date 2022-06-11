b959c8f7c19ee371ad27e9825328fc4612544f51
Minung Han <hmu332233@gmail.com>
2022-06-11 14:39:58 +0900

    ShareModal 추가

---
+++ packages/page/src/components/ShareModal.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+type Props = {
+  isOpen: boolean;
+  toggle: () => void;
+  code: string;
+};
+
+function ShareModal({ isOpen, toggle, code }: Props) {
+  return (
+    <>
+      <input
+        type="checkbox"
+        id="share-modal"
+        className="modal-toggle"
+        checked={isOpen}
+        onChange={toggle}
+      />
+      <label htmlFor="share-modal" className="modal cursor-pointer">
+        <label className="modal-box relative" htmlFor="">
+          <h3 className="text-lg font-bold">Share</h3>
+          <p className="py-4">다음 코드를 동료에게 공유해주세요!</p>
+          <input
+            type="text"
+            value={code}
+            className="input input-bordered w-full max-w-xs"
+            disabled
+          />
+        </label>
+      </label>
+    </>
+  );
+}
+
+export default ShareModal;
