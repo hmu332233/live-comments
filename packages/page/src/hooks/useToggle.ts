c4ae55b5a149239418e153ca3f17d84b412506a8
Minung Han <hmu332233@gmail.com>
2022-06-11 14:43:14 +0900

    useToggle에 타입을 const로 변경

--- packages/page/src/hooks/useToggle.ts
+++ packages/page/src/hooks/useToggle.ts
@@ -3,7 +3,7 @@ import { useState, useCallback } from 'react';
 function useToggle(initStatus = false) {
   const [value, setValue] = useState(initStatus);
   const toggle = useCallback(() => setValue((v) => !v), []);
-  return [value, toggle, setValue];
+  return [value, toggle, setValue] as const;
 }
 
 export default useToggle;
