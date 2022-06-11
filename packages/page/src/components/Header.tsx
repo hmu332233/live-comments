704c6668717885df104704e82a1b2c2c52f578ab
Minung Han <hmu332233@gmail.com>
2022-06-11 14:40:13 +0900

    Header 추가

---
+++ packages/page/src/components/Header.tsx
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import GithubIcon from './GithubIcon';
+import ShareModal from './ShareModal';
+import useToggle from '../hooks/useToggle';
+import { AuthStateContext } from '../contexts/AuthContext';
+
+type Props = {};
+
+function Header({}: Props) {
+  const auth = useContext(AuthStateContext)!;
+  const [isOpen, toggle] = useToggle();
+  return (
+    <>
+      <div className="flex items-center w-full h-12 border-b bg-neutral text-neutral-content p-2">
+        <span className="font-extrabold">Live Comment</span>
+        <div className="flex gap-2 ml-auto">
+          <button
+            className="btn btn-xs btn-outline btn-primary"
+            onClick={toggle}
+          >
+            Share
+          </button>
+          <GithubIcon />
+        </div>
+      </div>
+      <ShareModal isOpen={isOpen} toggle={toggle} code={auth.page.code} />
+    </>
+  );
+}
+
+export default Header;
