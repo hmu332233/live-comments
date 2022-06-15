b7df5ff19103b57164e858aa97c44bcd7d5b8a9a
Minung Han <hmu332233@gmail.com>
2022-06-16 00:58:08 +0900

    extension.app - ShareModal에 복사하기 버튼 추가

--- packages/extension.app/src/components/ShareModal.tsx
+++ packages/extension.app/src/components/ShareModal.tsx
@@ -7,6 +7,21 @@ type Props = {
 };
 
 function ShareModal({ isOpen, toggle, code }: Props) {
+  const url = `https://live-comments.minung.dev/code/${code}`;
+
+  const handleCopy = async () => {
+    if (typeof navigator.share !== 'undefined') {
+      await navigator.share({
+        title: `Live Comments - ${code}`,
+        url,
+      });
+    } else {
+      // share api 지원하지 않을시 clipboard 복사로 대체
+      await navigator.clipboard.writeText(url);
+      alert('복사되었습니다.');
+    }
+  };
+
   return (
     <>
       <input
@@ -20,12 +35,31 @@ function ShareModal({ isOpen, toggle, code }: Props) {
         <label className="modal-box relative" htmlFor="">
           <h3 className="text-lg font-bold">Share</h3>
           <p className="py-4">다음 URL을 동료에게 공유해주세요!</p>
-          <input
-            type="text"
-            value={`https://live-comments.minung.dev/code/${code}`}
-            className="input input-bordered w-full max-w-xs"
-            disabled
-          />
+          <div className="input-group">
+            <input
+              type="text"
+              value={url}
+              className="input input-bordered w-full cursor-pointer"
+              disabled
+            />
+            <button className="btn btn-square" onClick={handleCopy}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-copy"
+              >
+                <rect x="9" y="9" width="13" height="13" rx="2" ry="2" />
+                <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" />
+              </svg>
+            </button>
+          </div>
         </label>
       </label>
     </>
